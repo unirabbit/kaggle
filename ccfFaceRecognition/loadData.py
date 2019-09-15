2a8470ff790784ee75eaf696fb541313e1120ecc
adam <377926351@qq.com>
2019-09-15 21:34:49 +0800

    ccf人种识别代码

--- ccfFaceRecognition/loadData.py
+++ ccfFaceRecognition/loadData.py
@@ -14,11 +14,20 @@ def list_all_files(rootdir):
             _files.append(path)
     return _files
 
+
 def load_data(path):
-    file_list = list_all_files('./training/African')
+    file_list = list_all_files(path)
     X_train = np.empty((len(file_list), 112, 96, 3), dtype='uint8')
+    dict = {'African': 0, 'Asian': 1, 'Caucasian': 2, 'Indian': '3'}
+    Y_train_list = []
     # x_train = mpimg.imread(path）
     for i, file in enumerate(file_list):
         X_train[i, :, :, :] = mpimg.imread(file)
-    print(X_train.shape)
+        Y_train_list.append(dict[file.split('\\')[-3]])
+    Y_train = np.array(Y_train_list)
+    return X_train, Y_train
 
+
+X_train, Y_train = load_data('./training')
+print(f"X_train' shape is {X_train.shape}")
+print(f"Y_train' shape is {Y_train.shape}")
